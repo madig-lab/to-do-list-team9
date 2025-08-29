0b3306e71c31da3bf472936192cfa731cdc50b22
madig-lab <madison.m.graham24@gmail.com>
2025-08-29 09:22:19 -0500

    added delete functionality

--- todo.py
+++ todo.py
@@ -10,9 +10,10 @@ def add_task(task):
 # Step 3: View tasks
 def view_tasks():
     for i, task in enumerate(tasks, start=1):
-        print(f"{1} . {task}")
+        print(f"{i} . {task}")
 # Step 4: Delete a task
-
+def delete_task(remove):
+    tasks.pop(remove)
 
 # Step 5: Mark task complete
 
@@ -24,6 +25,8 @@ def view_tasks():
 if __name__ == "__main__":
     add_task("Finish Cyber 201 assignment")
     add_task("Push code to GitHub")
+    view_tasks()
+    delete_task(0)
     view_tasks()
    # mark_complete(0)
     #view_tasks()
